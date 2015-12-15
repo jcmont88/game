fcfd6732d601bf898f29e58d40f0d7f6841fe097
Nesly Mejia <Nesly@Neslys-MacBook-Air.local>
2015-12-15 15:16:11 -0500

    Second commit

---
+++ untitled.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+
+class Caracther:
+  def __init__(self):
+  	self.attack_weight = 0
+  	self.defense_hit_weight = 0
+  	self.attack_damage_weight = 0
+  	self.defense_damage_weight = 0
+  	self.health = 0
+  	self.items = None
+  	self.experience 
+
+class Operations:
+	def __init__(self):
+
+
+		
+print 'Hello World'
+
+#caracter class and its test
+#mock and it test
