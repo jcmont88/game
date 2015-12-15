49aac2858d6355728c7c6784de85c597b475c073
Nesly Mejia <Nesly@Neslys-MacBook-Air.local>
2015-12-15 15:05:14 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+
+class Caracther:
+  def __init__(self):
+    self.name = None
+    self.logo = None
+    self.members = 0
+
+print 'Hello World'
+
+#caracter class and its test
+#mock and it test
